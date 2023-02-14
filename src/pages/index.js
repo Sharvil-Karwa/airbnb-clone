31ebff101982e09aef362f9a4ba4fe88996c6b24
Sharvil <karwasharvil@gmail.com>
2023-02-14 09:30:47 +0530

    added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Banner from "components/Banner";
 import Header from "components/Header";
 import Head from "next/head";
-import mockData from "mockData";
+import mockData from "/lib/data/mockData";
+import mockData2 from "/lib/data/mockData2";
 import SmallCard from "components/SmallCard";
+import MediumCard from "components/MediumCard";
+import LargeCard from "components/LargeCard";
+import Footer from "components/Footer";
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardData }) {
   return (
     <div>
       <Head>
@@ -27,16 +31,35 @@ export default function Home({ exploreData }) {
             ))}
           </div>
         </section>
+        <section>
+          <h2 className="text-4xl font-semibold pb-5">Live anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <section>
+          <LargeCard
+            image="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Wishlists created by Airbnb"
+            buttonText="Get Inspired"
+          />
+        </section>
       </main>
+      <Footer />
     </div>
   );
 }
 
 export async function getStaticProps() {
   const exploreData = mockData;
+  const cardData = mockData2;
   return {
     props: {
       exploreData,
+      cardData,
     },
   };
 }
